b199d4b1a13a5bbda828605af5dcfe606089c67e
rhackerd <josefduda18@gmail.com>
2025-12-13 13:39:19 +0100

    Added simpler macros when using NOVA_LOG_DEF

--- root/core/include/Nova/Core/core.h
+++ root/core/include/Nova/Core/core.h
@@ -22,6 +22,8 @@ struct ModuleContext {
 
 namespace Nova::Core {
     #define NOVA_CORE_LOGGING true
+
+    #if NOVA_CORE_LOGGING
     class Logger {
     private:
         Nova::Logger log;
@@ -49,11 +51,43 @@ namespace Nova::Core {
             log.debug(fmt::format(fmtStr, std::forward<Args>(args)...));
         }
     };
+    #else
+    class NullLogger {
+    private:
+
+
+    public:
+        explicit Logger() {}
+
+        template<typename... Args>
+        inline void Info(fmt::format_string<Args...> fmtStr, Args&&... args) {
+            log.info(fmt::format(fmtStr, std::forward<Args>(args)...));
+        }
+
+        template<typename... Args>
+        inline void Warn(fmt::format_string<Args...> fmtStr, Args&&... args) {
+            log.warn(fmt::format(fmtStr, std::forward<Args>(args)...));
+        }
+
+        template<typename... Args>
+        inline void Error(fmt::format_string<Args...> fmtStr, Args&&... args) {
+            log.error(fmt::format(fmtStr, std::forward<Args>(args)...));
+        }
+
+        template<typename... Args>
+        inline void Debug(fmt::format_string<Args...> fmtStr, Args&&... args) {
+            log.debug(fmt::format(fmtStr, std::forward<Args>(args)...));
+        }
+    };
+    #endif
+
+
     #if NOVA_CORE_LOGGING
         #define NOVA_INFO(logger, fmt, ...)  (logger).Info(fmt, ##__VA_ARGS__)
         #define NOVA_WARN(logger, fmt, ...)  (logger).Warn(fmt, ##__VA_ARGS__)
         #define NOVA_ERROR(logger, fmt, ...) (logger).Error(fmt, ##__VA_ARGS__)
         #define NOVA_DEBUG(logger, fmt, ...) (logger).Debug(fmt, ##__VA_ARGS__)
+        #define NINFO(fmt, ...) NOVA_INFO(oLogger, fmt, ##__VA_ARGS__)
     #else
         #define NOVA_INFO(logger, fmt, ...)  (void)0
         #define NOVA_WARN(logger, fmt, ...)  (void)0
@@ -64,7 +98,7 @@ namespace Nova::Core {
     #if NOVA_CORE_LOGGING
         #define NOVA_LOG_DEF(name) Nova::Core::Logger oLogger{name};
     #else
-        #define NOVA_LOG_DEF(name)
+        #define NOVA_LOG_DEF(name) 
     #endif
 
     class Object {
---
+++ root/core/include/Nova/Core/macros.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+#if NOVA_CORE_LOGGING
+
+    #define NOVA_INFO(logger, fmt, ...)   (logger).Info((fmt), ##__VA_ARGS__)
+    #define NOVA_WARN(logger, fmt, ...)   (logger).Warn((fmt), ##__VA_ARGS__)
+    #define NOVA_ERROR(logger, fmt, ...)  (logger).Error((fmt), ##__VA_ARGS__)
+    #define NOVA_DEBUG(logger, fmt, ...)  (logger).Debug((fmt), ##__VA_ARGS__)
+
+    #define NINFO(fmt, ...)   NOVA_INFO(oLogger, fmt, ##__VA_ARGS__)
+    #define NWARN(fmt, ...)   NOVA_WARN(oLogger, fmt, ##__VA_ARGS__)
+    #define NERROR(fmt, ...)  NOVA_ERROR(oLogger, fmt, ##__VA_ARGS__)
+    #define NDEBUG(fmt, ...)  NOVA_DEBUG(oLogger, fmt, ##__VA_ARGS__)
+
+#else
+
+    #define NOVA_INFO(logger, fmt, ...)   ((void)0)
+    #define NOVA_WARN(logger, fmt, ...)   ((void)0)
+    #define NOVA_ERROR(logger, fmt, ...)  ((void)0)
+    #define NOVA_DEBUG(logger, fmt, ...)  ((void)0)
+
+    #define NINFO(fmt, ...)    ((void)0)
+    #define NWARN(fmt, ...)    ((void)0)
+    #define NERROR(fmt, ...)   ((void)0)
+    #define NDEBUG(fmt, ...)   ((void)0)
+
+#endif
+
+#define NOVA_LOG_DEF(name) ::Nova::Core::Logger oLogger{(name)}
